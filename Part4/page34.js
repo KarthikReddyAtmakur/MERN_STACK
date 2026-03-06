192a3bb0271652254fce0b0d6f31cf135f85a8ed
KarthikReddyAtmakur <karthikreddy25.09.2005@gmail.com>
2026-03-06 10:51:54 +0530

    updated

--- Part4/page34.js
+++ Part4/page34.js
@@ -3,6 +3,12 @@ const app = express()
 app.set("view engine","ejs")
 app.set("views","views")
 app.listen(8080, ()=> console.log("Server Started"));
+
+const users = [
+    {name:"Karthik", email:"karthik@gmail.com", password: "1234"},
+    {name:"Komal", email:"komal@gmail.com", password: "1234"},
+    {name:"Karan", email:"karan@gmail.com", password: "1234"},
+];
 app.get("/login",(req,res)=> {
     res.render("login");
 });
@@ -10,5 +16,5 @@ app.get("/register",(req,res)=> {
     res.render("register");
 });
 app.get("/",(req,res)=> {
-    res.render("dashboard");
+    res.render("dashboard",{users});
 });
