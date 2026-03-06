6a38779aab5a5fadb5197f5b7fb2a3194ae5e75c
KarthikReddyAtmakur <karthikreddy25.09.2005@gmail.com>
2026-03-06 09:59:12 +0530

    Initial MERN stack project upload

---
+++ Part4/calc.js
@@ -0,0 +1,12 @@
+export function add(x, y) {
+  console.log(x + y);
+}
+
+export function subtract(x, y) {
+  console.log(x - y);
+}
+// export {add,subtract}
+/* export function add(x, y) {
+  console.log(x + y);
+} can import the function without {} => import add from ".calc.js",
+works only when their is only one function */
---
+++ Part4/page33.js
@@ -0,0 +1,3 @@
+import {add,subtract} from "./calc.js"
+add(2, 4);
+subtract(7, 5);
